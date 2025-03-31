30a080b6972bd02c0c8bce6f72a96f43dfa1b6ea
VidhiDew <vidhidewangan001@gmail.com>
2025-03-31 12:24:04 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -1,10 +1,12 @@
 class Demo{
+  Public static void main(String[] args){
   int a= 10;
   int b= 20;
   int result
   result = a;
   a = b;
   b = result;
-  
+    System.out.println("a = "+a+"b = "+b);
+  }
   
 }
